a230cb30dfdb81ea9b3f599ff0394422698b8af5
SyoInoue <funnyfacee7777@gmail.com>
2023-09-09 17:12:31 +0900

    levaの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
 function App() {
+  const { scale, positionX, positionY, positionZ, wireFrame, color } =
+    useControls("Box", {
+      scale: {
+        value: 0,
+        min: 1,
+        max: 3,
+        step: 0.1,
+      },
+      positionX: {
+        value: 0,
+        min: -3,
+        max: 3,
+        step: 0.1,
+      },
+      positionY: {
+        value: 0,
+        min: -3,
+        max: 3,
+        step: 0.1,
+      },
+      positionZ: {
+        value: 0,
+        min: -3,
+        max: 3,
+        step: 0.1,
+      },
+      color: "#f00",
+      wireFrame: false,
+    });
   return (
     <div className="w-full h-screen">
       <Canvas>
         <ambientLight intensity={0.1} />
         <directionalLight position={[0, 0, 5]} />
-        <Box args={[2, 2, 2]}>
-          <meshStandardMaterial color="red" />
+        <Box
+          args={[2, 2, 2]}
+          scale={scale}
+          position={[positionX, positionY, positionZ]}
+        >
+          <meshStandardMaterial color={color} wireframe={wireFrame} />
         </Box>
         <OrbitControls />
       </Canvas>
