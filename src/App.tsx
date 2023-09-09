be93cfda9c3a3db03f20aafdb474c5c1a7f97f66
SyoInoue <funnyfacee7777@gmail.com>
2023-09-09 17:27:03 +0900

    resetbutton追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,52 @@
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
-import { useControls } from "leva";
+import { useControls, folder, button } from "leva";
 
 function App() {
-  const { scale, positionX, positionY, positionZ, wireFrame, color } =
-    useControls("Box", {
-      scale: {
-        value: 0,
-        min: 1,
-        max: 3,
-        step: 0.1,
-      },
-      positionX: {
-        value: 0,
-        min: -3,
-        max: 3,
-        step: 0.1,
-      },
-      positionY: {
-        value: 0,
-        min: -3,
-        max: 3,
-        step: 0.1,
-      },
-      positionZ: {
-        value: 0,
-        min: -3,
-        max: 3,
-        step: 0.1,
-      },
-      color: "#f00",
-      wireFrame: false,
-    });
+  const [{ scale, positionX, positionY, positionZ, wireFrame, color }, reset] =
+    useControls("Box", () => ({
+      geometry: folder({
+        scale: {
+          value: 0,
+          min: 1,
+          max: 3,
+          step: 0.1,
+        },
+        positionX: {
+          value: 0,
+          min: -3,
+          max: 3,
+          step: 0.1,
+        },
+        positionY: {
+          value: 0,
+          min: -3,
+          max: 3,
+          step: 0.1,
+        },
+        positionZ: {
+          value: 0,
+          min: -3,
+          max: 3,
+          step: 0.1,
+        },
+      }),
+      material: folder({
+        color: "#f00",
+        wireFrame: false,
+      }),
+      reset: button(() => {
+        reset({
+          scale: 1,
+          positionX: 0,
+          positionY: 0,
+          positionZ: 0,
+          color: "#f00",
+          wireFrame: false,
+        });
+      }),
+    }));
+
   return (
     <div className="w-full h-screen">
       <Canvas>
