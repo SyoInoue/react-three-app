8e30336f909183c5f3d04b2320dc5642d41438c4
SyoInoue <funnyfacee7777@gmail.com>
2023-09-09 16:56:12 +0900

    四角オブジェクト追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="w-full h-screen">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} />
+        <Box args={[2, 2, 2]}>
+          <meshStandardMaterial color="red" />
+        </Box>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
